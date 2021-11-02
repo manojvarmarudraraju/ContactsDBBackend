7b2dc530740b3317da67b8f945f6114736127124
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-11-02 08:46:34 -0500

    enabling cors

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ var connStr = "DATABASE="+config.dbname+ ";" +
                 "PWD="+ config.password+";" +
                 "Security="+ config.Security;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST'); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers","*")
+  next();
+});
 app.use(function(req, res, next) {
   logger.info(req.url)
   next()
